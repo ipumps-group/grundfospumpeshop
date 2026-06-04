8e620c8d05132adb9ebb71097c08a8d618448a84
RonaldOutline <85238050+RonaldOutline@users.noreply.github.com>
2026-06-04 15:00:53 +0300

    Add Meta Pixel tracking code to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import { cookies } from 'next/headers'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Analytics } from '@vercel/analytics/next'
+import Script from 'next/script'
 import './globals.css'
 
 const inter = Inter({
@@ -84,8 +85,21 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     <html lang={locale} suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://sdqnzyfmanflslsjhytf.supabase.co" />
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init','2133761077401963');
+fbq('track','PageView');`}
+        </Script>
       </head>
       <body className={inter.className}>
+        <noscript><img height="1" width="1" style={{display:'none'}} src="https://www.facebook.com/tr?id=2133761077401963&ev=PageView&noscript=1" /></noscript>
         {children}
         <SpeedInsights />
         <Analytics />
